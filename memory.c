964082c1543cdfe1e82b5fcfb927dc47a21936bd
SukuPaperu <mrpaperdisc@gmail.com>
2020-12-18 19:08:12 +0100

    1ère implémentation de la mémoire (fonctionnelle)

--- memory.c
+++ memory.c
@@ -20,45 +20,138 @@ Contact: Guillaume.Huard@imag.fr
 	 700 avenue centrale, domaine universitaire
 	 38401 Saint Martin d'Hères
 */
+
+// Les pages 68 à 71 sont dédiées à l'espace mémoire et la gestion des adresses
+
 #include <stdlib.h>
 #include "memory.h"
 #include "util.h"
 
 struct memory_data {
+    // Type de boutisme de la mémoire
+    int is_big_endian_type;
+    // Taille de la mémoire (en octets)
+    int size;
+    // Espace mémoire (sous forme d'entiers non signés sur 8 bits)
+    uint8_t* data;
 };
 
 memory memory_create(size_t size, int is_big_endian) {
-    memory mem=NULL;
+    memory mem = malloc(sizeof(memory));
+
+    if (mem != NULL && size > 0) {
+        mem->data = calloc(size, sizeof(uint8_t));
+        mem->size = size;
+        mem->is_big_endian_type = is_big_endian;
+    }
+
+    // Si l'allocation échoue ou que la taille est invalide on libère les espaces mémoires précédemment alloués
+    if (mem->data == NULL) {
+        free(mem->data);
+        free(mem);
+        mem = NULL;
+    }
+
     return mem;
 }
 
 size_t memory_get_size(memory mem) {
-    return 0;
+    return mem->size;
 }
 
 void memory_destroy(memory mem) {
+    free(mem->data);
+    free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+    // si on sélectionne une adresse en dehors de l'espace adressable
+    if(address > mem->size)
+        return 0;
+
+    *value = mem->data[address];
+
+    return 1;
 }
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value) {
-    return -1;
+    // si l'adresse n'est pas alignée sur un half ou que l'on sélectionne une adresse en dehors de l'espace adressable
+    if(get_bit(address, 0) != 0 || address + 1 > mem->size)
+        return 0;
+
+    // en fonction du type de boutisme de mem
+    if(mem->is_big_endian_type)
+        *value = (mem->data[address] << 8) | mem->data[address + 1];
+    else
+        *value = (mem->data[address + 1] << 8) | mem->data[address];
+
+    return 1;
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
-    return -1;
+    // si l'adresse n'est pas alignée sur un word ou que l'on sélectionne une adresse en dehors de l'espace adressable
+    if(get_bits(address, 1, 0) != 0 || address + 3 > mem->size)
+        return 0;
+
+    // en fonction du type de boutisme de mem
+    if(mem->is_big_endian_type)
+        *value = (mem->data[address] << 24) 
+            | (mem->data[address + 1] << 16) 
+            | (mem->data[address + 2] << 8) 
+            | mem->data[address + 3];
+    else
+        *value = (mem->data[address + 3] << 24) 
+            | (mem->data[address + 2] << 16) 
+            | (mem->data[address + 1] << 8) 
+            | mem->data[address];
+
+    return 1;
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    return -1;
+    // si on sélectionne une adresse en dehors de l'espace adressable
+    if(address > mem->size)
+        return 0;
+
+    mem->data[address] = value;
+
+    return 1;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value) {
-    return -1;
+    // si l'adresse n'est pas alignée sur un half ou que l'on sélectionne une adresse en dehors de l'espace adressable
+    if(get_bit(address, 0) != 0 || address + 1 > mem->size)
+        return 0;
+
+    // en fonction du type de boutisme de mem
+    if(mem->is_big_endian_type) {
+        mem->data[address] = value >> 8;
+        mem->data[address + 1] = value;
+    } else {
+        mem->data[address + 1] = value >> 8;
+        mem->data[address] = value;
+    }
+
+    return 1;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value) {
-    return -1;
+    // si l'adresse n'est pas alignée sur un word ou que l'on sélectionne une adresse en dehors de l'espace adressable
+    if(get_bits(address, 1, 0) != 0 || address + 3 > mem->size)
+        return 0;
+
+    // en fonction du type de boutisme de mem
+    if(mem->is_big_endian_type) {
+        mem->data[address] = value >> 24;
+        mem->data[address + 1] = value >> 16;
+        mem->data[address + 2] = value >> 8;
+        mem->data[address + 3] = value;
+    } else {
+        mem->data[address + 3] = value >> 24;
+        mem->data[address + 2] = value >> 16;
+        mem->data[address + 1] = value >> 8;
+        mem->data[address] = value;
+    }
+
+    return 1;
 }
