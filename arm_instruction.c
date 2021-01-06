f81dffd3647874860ec17444bd38ac9e56179f3b
SukuPaperu <mrpaperdisc@gmail.com>
2021-01-06 15:47:37 +0100

    Correctif mineur permettant la compilation ("return 0;" à la fin de "inst_cond()")

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -43,7 +43,7 @@ int arm_step(arm_core p) {
 
 
 int inst_cond(arm_core p, uint32_t ins) {
-    uint32_t flags = read_cpsr(p->reg);
+    uint32_t flags = arm_read_cpsr(p);
 
     uint32_t flag_N = (flags >> N) & 1;
     uint32_t flag_Z = (flags >> Z) & 1;
@@ -128,9 +128,11 @@ int inst_cond(arm_core p, uint32_t ins) {
             return 1;
             break;
 
-        // instruction inconditionnelle (aucune du genre n'est implémentée ici, le cas est traité comme AL)
+        // instruction inconditionnelle (aucune du genre n'est implï¿½mentï¿½e ici, le cas est traitï¿½ comme AL)
         case 0xF:
-        return 1;
+            return 1;
             break;
-    }    
+    }
+
+    return 0;
 }
