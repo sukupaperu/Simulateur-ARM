798d5e6b51149d6e33405b59a6e1dea2146cad00
Mohdoo <marinszcz@gmail.com>
2021-01-11 15:34:57 +0100

    Tests pour les instructions de comparaison + typo sur ex_BIC

--- Examples/ex_TEQ.s
+++ Examples/ex_TEQ.s
@@ -2,7 +2,9 @@
 .text
 main:
 	mov r0, #0xc
-	mov r1, #0x9
-	bics r2, r0, r1
+	mov r1, #0xc
+	teq r0, r1
+	mov r1, #0xd
+	teq r0, r1
 end:
 	swi 0x123456
--- Examples/ex_TST.s
+++ Examples/ex_TST.s
@@ -3,6 +3,6 @@
 main:
 	mov r0, #0xc
 	mov r1, #0x9
-	bics r2, r0, r1
+	tst r0, r1
 end:
 	swi 0x123456
