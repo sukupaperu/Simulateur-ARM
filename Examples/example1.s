deb68e76964ac9aed9468e5f5ae508ee7fbf5754
Mohdoo <marinszcz@gmail.com>
2021-01-13 11:51:21 +0100

    Commentaire de example1.s

--- Examples/example1.s
+++ Examples/example1.s
@@ -1,5 +1,10 @@
 .global main
 .text
+
+    # test de B et BL
+    # résultat attendu : le programme se termine lorsque r0 == 0
+    # on passe cinq fois dans la boucle decr
+    # à chaque passage on peut observer la mise à jour de r14 par bl
 decr:
     subs r0, r0, #1
     mov pc, lr
